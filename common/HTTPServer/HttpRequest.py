83039a8b85840b73d459841f51f81f59b3159efd
“e <18790334713@163.com>
2020-11-18 21:08:10 +0800

    修改微信签名

--- common/HTTPServer/HttpRequest.py
+++ common/HTTPServer/HttpRequest.py
@@ -102,8 +102,8 @@ def req_wx_asset_token(_code):
     微信登录配置
     """
     # 发送请求
-    _app_id = "wx88ab5f31ca993bc8"
-    _secret = "1078be709c7976c67c2026b612db2fa4"
+    _app_id = "wxfb4a9b8046f5a297"
+    _secret = "b608b87d3835c5c555042e662c49c132"
     _url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code" % (
         _app_id, _secret, _code)
     context = ssl._create_unverified_context()
